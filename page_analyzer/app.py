f886fec9df3b4d82143f592799a43ddcdc8d4411
etogit <sturik@bk.ru>
2023-07-15 14:14:56 +0300

    add *

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
-load_dotenv(find_dotenv())
 DATABASE_URL = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 app.config.update(SECRET_KEY=SECRET_KEY)
